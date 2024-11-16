32f42d272b822c83b680767b5ba1dd2b85880d40
Your Name Goes Here <harshakondaveeti05@gmail.com>
2024-11-16 22:16:38 +0530

    bt

--- trees/binarytree.cpp
+++ trees/binarytree.cpp
@@ -15,6 +15,33 @@ struct Node
 
 };
 
+void preorder(Node* root){
+    if (root!=NULL)
+    {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    
+}
+void inorder(Node* root){
+    if (root!=NULL)
+    {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+    
+}
+void postorder(Node* root){
+    if (root!=NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+    
+}
 
 int main(){
 
@@ -24,6 +51,9 @@ int main(){
 
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    
+
+    // preorder(root);
+    // inorder(root);
+    // postorder(root);
     return 0;
 }
